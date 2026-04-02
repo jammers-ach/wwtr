62c844d3011a64f07a35ec7afaa8307c49856888
jammers <james@mathews.fi>
2026-04-03 00:23:31 +0200

    fixed generate command for rather cards

--- cards/rather/generate.sh
+++ cards/rather/generate.sh
@@ -1,53 +1,43 @@
 #!/bin/bash
 
+
 set -euo pipefail
 
-set -x
-
-# File paths
 INPUT="list.txt"
 TEMPLATE="A5_rather_template.svg"
-
-# Counter for sheet number
 sheet_num=1
 
 
+
 # Read input.txt in chunks of 8 lines
-while IFS= read -r line1 && IFS= read -r line2 && IFS= read -r line3 && \
-      IFS= read -r line4 && IFS= read -r line5 && IFS= read -r line6 && \
-      IFS= read -r line7 && IFS= read -r line8; do
+lines_buffer=()
+while IFS= read -r line; do
+    lines_buffer+=("$line")
+    if [ "${#lines_buffer[@]}" -eq 8 ]; then
+        name="sheet_${sheet_num}.svg"
+        cp "$TEMPLATE" "$name"
 
-    # Copy template to new sheet file
-    OUTFILE="A5_rather_${sheet_num}.svg"
-    cp "$TEMPLATE" "$OUTFILE"
-
-    # Replace placeholders with lines
-    sed -i \
-        -e "s/Text 1/$line1/" \
-        -e "s/Text 2/$line2/" \
-        -e "s/Text 3/$line3/" \
-        -e "s/Text 4/$line4/" \
-        -e "s/Text 5/$line5/" \
-        -e "s/Text 6/$line6/" \
-        -e "s/Text 7/$line7/" \
-        -e "s/Text 8/$line8/" \
-        "$OUTFILE"
-
-    ((sheet_num++))
+        text_num=1
+        for line in "${lines_buffer[@]}"; do
+            sed -i "s|Text $text_num|$(printf '%s' "$line" | sed 's/[&/\]/\\&/g')|g" "$name"
+            echo "$sheet_num.$text_num: $line"
+            ((text_num++))
+        done
+        ((sheet_num++))
+        lines_buffer=()
+    fi
 done < "$INPUT"
 
-# Handle remaining lines if input.txt is not a multiple of 8
-remaining_lines=()
-while IFS= read -r line; do
-    remaining_lines+=("$line")
-done
+# Handle remaining lines (less than 8)
+if [ "${#lines_buffer[@]}" -gt 0 ]; then
+    name="sheet_${sheet_num}.svg"
+    cp "$TEMPLATE" "$name"
 
-if [ ${#remaining_lines[@]} -gt 0 ]; then
-    OUTFILE="A5_rather_${sheet_num}.svg"
-    cp "$TEMPLATE" "$OUTFILE"
-    for i in "${!remaining_lines[@]}"; do
-        placeholder=$((i+1))
-        sed -i "s/text $placeholder/${remaining_lines[i]}/" "$OUTFILE"
+    text_num=1
+    for line in "${lines_buffer[@]}"; do
+        sed -i "s|Text $text_num|$(printf '%s' "$line" | sed 's/[&/\]/\\&/g')|g" "$name"
+        echo "$sheet_num.$text_num: $line"
+        ((text_num++))
     done
 fi
 
